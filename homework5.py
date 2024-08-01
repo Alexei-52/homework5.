ea38f4713cd57af1c1b7cfd33910c6c25fbc1a3f
Alexei-52 <rod.leha2014@yandex.ru>
2024-08-01 11:45:28 +0300

    Update homework5.py

--- homework5.py
+++ homework5.py
@@ -1,5 +1,7 @@
 immutable_var = (['a','b'],1,2)
 print("Незменяемый объект 'Кортеж':",immutable_var)
+#immutable_var[2] = 55
+print('Попробовал изменить, выводит ошибку так как в уроке было уже сказано что "кортежи" неизменяемые списки')
 print(type(immutable_var))
 mutable_list = ['a','b',1,2]
 print("Изменяемый объект 'Список':",mutable_list)
